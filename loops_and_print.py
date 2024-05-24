500f6ab0d35cbd0b743cbca633ca12c010b24ae2
MRecioO <162721939+MRecioO@users.noreply.github.com>
2024-05-23 21:19:42 -0300

    Update loops_and_print.py

--- loops_and_print.py
+++ loops_and_print.py
@@ -1,6 +1,23 @@
 def enumerate_list(list):
-    return list
-
+    nlist = []
+    if list != []:
+        i = 0 
+        for elem in list:
+            if  elem != '':
+                nlist.append(f'{i}. {elem}')
+                i += 1
+        return  nlist
+    else:
+        return list
 
 def enumerate_backwards(list):
-    return list
+    nlist = []
+    if list != []:
+        i = 0 
+        for elem in list:
+            if elem != '':
+                nlist.append(f'{i}. {elem[::-1]}')
+                i += 1
+        return nlist
+    else:
+        return list
