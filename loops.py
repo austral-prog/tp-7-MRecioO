76cddcc508b1f75244b15a45330eab905ce8d570
MRecioO <162721939+MRecioO@users.noreply.github.com>
2024-05-23 21:20:17 -0300

    Update loops.py

--- loops.py
+++ loops.py
@@ -1,18 +1,54 @@
 def index_of_by_index(word, list, index):
-    return -1
-
+    i = -1
+    nlist = []
+    for elem in list[index:]:
+        if elem != word:
+            nlist.append(elem)
+        else:
+            i = len(nlist) + len(list[:index])
+            break
+    return i
 
 def index_of_empty(list):
-    return -1
-
+    i = -1
+    nlist = []
+    for elem in list:
+        if elem != '':
+            nlist.append(elem)
+        else:
+            i = len(nlist)
+            break
+    return i
 
 def index_of(word, list):
-    return -1
-
+    i = -1
+    nlist = []
+    for elem in list:
+        if elem != word:
+            nlist.append(elem)
+        else:
+            i = len(nlist)
+            break
+    return i
 
 def put(word, list):
-    return -1
-
+    i = 0
+    if '' in list:
+        for elem in list:
+            i += 1
+            if elem == '':
+                list.insert(i, word)
+                del list[i - 1]
+                i = i - 1
+                break
+        return i 
+    else:
+        return -1
 
 def remove(word, list):
-    return -1
+    i = 0
+    for item in list:
+        if item == word: 
+            list[index_of(word,list)] = ''
+            i += 1
+    return i
